a4f8e0b477b05aaf4da936123856b80b537e719c
Harrison Reich <hreich1998@gmail.com>
2021-11-24 10:49:10 -0800

    testing food api

---
+++ js/foodapi.js
@@ -0,0 +1,22 @@
+// gives pic, food item, and food name
+document.getElementById(`searchBtn`).addEventListener(`click`, event => {
+  event.preventDefault()
+  const ingredient = document.getElementById('ingredient').value
+
+  axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${ingredient}&apiKey=537e785a5f5e4d618536a1641da530d3`)
+    .then(res => {
+      const food = res.data
+      console.log(food)
+
+      let randomIndex = Math.floor(Math.random() * 10)
+
+      const currentElem = document.createElement('div')
+      currentElem.innerHTML = `
+        <h3>"${food.results[randomIndex].title}"</h3>
+        <img src="${food.results[randomIndex].image}" alt="${food.results[randomIndex].title}">
+      `
+
+      document.getElementById('recipe').append(currentElem)
+    })
+
+    // gives pic, food item, food name, and recipe
