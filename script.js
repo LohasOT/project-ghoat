4285735691a1fcc39bd49adc0a5f193fe7847fd7
Harrison Reich <hreich1998@gmail.com>
2021-11-24 11:54:59 -0800

    updated food api

--- script.js
+++ script.js
@@ -9,11 +9,14 @@ document.getElementById(`searchBtn`).addEventListener(`click`, event => {
       console.log(food)
 
       let randomIndex = Math.floor(Math.random() * 10)
+      let steps = food.results[randomIndex].anaylzedInstructions[0].steps
+      // need to make a forloop for steps and ingredients ^
 
       const currentElem = document.createElement('div')
       currentElem.innerHTML = `
         <h3>"${food.results[randomIndex].title}"</h3>
         <img src="${food.results[randomIndex].image}" alt="${food.results[randomIndex].title}">
+        <p>"${steps[0]}"</p>
         
       `
 
