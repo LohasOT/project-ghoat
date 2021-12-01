c0bebcb7abe77fbca01c3dc41b198f56b05ec376
LohasOT <oliverhoang93@gmail.com>
2021-12-01 11:26:35 -0800

    new key

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function getData() {
   const ingredient = document.getElementById('ingredient').value
 
 // getting information from spoonacular API
-  axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${ingredient}&addRecipeInformation=true&fillIngredients=true&instructionsRequired=true&apiKey=0ccd34341a57418f9bbc6d88b80a81e2`)
+  axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${ingredient}&addRecipeInformation=true&fillIngredients=true&instructionsRequired=true&apiKey=6b9d0e1539434e12ab8da9fd6d0a1184`)
     .then(function(res){
 
       food = res.data
