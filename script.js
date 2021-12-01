800475d64434d558b6fd0bf53a5b80ea31c4ff89
LohasOT <oliverhoang93@gmail.com>
2021-12-01 00:11:05 -0800

    commented on js

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
+// Declaring variables for local storages
 let movie
 let poster
 let food
 let foodObject
 
+// On click event
 document.getElementById(`searchBtn`).addEventListener(`click`, getData)
 
+// function to remove hide css and make the card appear
 function getData() {
+
   let cardDiv = document.getElementById("card");
   cardDiv.classList.remove("hide");
+
+// declaring ingredient value
+
   const ingredient = document.getElementById('ingredient').value
 
+// getting information from spoonacular API
   axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${ingredient}&addRecipeInformation=true&fillIngredients=true&instructionsRequired=true&apiKey=0ccd34341a57418f9bbc6d88b80a81e2`)
     .then(function(res){
 
       food = res.data
 
+// making random index for random food
+
       let randomIndex = Math.floor(Math.random() * 10)
+
+// declaring variables on the food contents
+
       let steps = food.results[randomIndex].analyzedInstructions[0].steps
       let ingredients = food.results[randomIndex].extendedIngredients
-
+// appending information into the innerHTML
       const foodPic = document.getElementById('foodPoster')
 
+// getting food informations into the card
       foodPic.innerHTML = `<img src="${food.results[randomIndex].image}" alt="${food.results[randomIndex].title}">`
 
 
@@ -29,7 +43,7 @@ function getData() {
             <h4>${food.results[randomIndex].title}</h4>
             <p>${food.results[randomIndex].summary}</p>
           `
-
+// looping the steps
       for (let i = 0; i < steps.length; i++) {
         const stepElem = document.createElement('li')
         stepElem.innerHTML = `
@@ -37,13 +51,13 @@ function getData() {
         `
 
       }
-
+// looping the ingredients
       for (let e = 0; e < ingredients.length; e++) {
         const ingredientsElem = document.createElement('li')
         ingredientsElem.innerHTML = `
         "${ingredients[e].original}"
         `
-
+// making key values on food to pull from local storage later
       }
       foodObject = {
         title: food.results[randomIndex].title,
@@ -53,19 +67,22 @@ function getData() {
       }
 
     })
+// getting information from the randomized movie API
   axios.get(`https://k2maan-moviehut.herokuapp.com/api/random`)
     .then(res => {
       movie = res.data
       let title = movie.name
 
-
+// getting poster and summary from the 2nd API
       axios.get(`https://www.omdbapi.com/?t=${title}&apikey=39892eb2`)
         .then(resp => {
+// declaring variables for movie
 
           poster = resp.data
           picture = poster.Poster
           descript = poster.Plot
 
+// appending movie information into the innerHTML
           const moviepic = document.getElementById('moviePoster')
 
           moviepic.innerHTML = `<img src="${picture}">`
@@ -88,21 +105,25 @@ function getData() {
         })
     })
 }
-
+// onclick function when saveCombo is clicked
 document.getElementById('saveCombo').addEventListener('click', event => {
-
+// making localStorage for both movie and food API
   event.preventDefault()
   let myStoredPoster = JSON.parse(localStorage.getItem('myPoster')) || []
   let myStoredMovie = JSON.parse(localStorage.getItem('myMovie')) || []
   let myStoredFood = JSON.parse(localStorage.getItem('myFood')) || []
+// pushing into localStorage
 
   myStoredMovie.push(movie)
   myStoredPoster.push(poster)
   myStoredFood.push(foodObject)
+// stringifying the contents
 
   localStorage.setItem('myMovie', JSON.stringify(myStoredMovie))
   localStorage.setItem('myPoster', JSON.stringify(myStoredPoster))
   localStorage.setItem('myFood', JSON.stringify(myStoredFood))
+
+// bring the user over to myResults.html
   window.location.href = "myResults.html"
 
 })
